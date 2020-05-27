c3de426847e952acfcedfac72436a071bb365347
Brent Bettis <bbettis@expedia.com>
2020-05-27 13:13:12 -0700

    Start of WebGL rendering

--- www/index.js
+++ www/index.js
@@ -1,14 +1,241 @@
 import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const CELL_SIZE = 5; // px
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
+main();
 
-const universe = Universe.new();
-const width = universe.width();
-const height = universe.height();
+function main() {
+    const CELL_SIZE = 5; // px
+    const GRID_COLOR = "#CCCCCC";
+    const DEAD_COLOR = "#FFFFFF";
+    const ALIVE_COLOR = "#000000";
+
+    const universe = Universe.new();
+    const width = universe.width();
+    const height = universe.height();
+
+    const canvas = document.getElementById("game-of-life-canvas");
+    // const ctx = canvas.getContext('2d');
+
+    canvas.height = (CELL_SIZE + 1) * height + 1;
+    canvas.width = (CELL_SIZE + 1) * width + 1;
+    canvas.addEventListener("click", event => {
+        const insertPopulation = document.getElementById("insert").value;
+        const hFlip = document.getElementById("h-flip").checked;
+        const vFlip = document.getElementById("v-flip").checked;
+        const invert = document.getElementById("invert").checked;
+
+        const boundingRect = canvas.getBoundingClientRect();
+
+        const scaleX = canvas.width / boundingRect.width;
+        const scaleY = canvas.height / boundingRect.height;
+
+        const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+        const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+        const row = (Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1) + height) % height;
+        const col = (Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1) + width) % width;
+
+        if (insertPopulation === "toggle") {
+            universe.toggle_cell(row, col);
+        } else {
+            universe.seed_population(row, col, insertPopulation, hFlip, vFlip, invert);
+        }
+
+        drawGrid();
+        drawCells();
+    });
+
+    const gl = canvas.getContext('webgl');
+    // If we don't have a GL context, give up now
+
+    if (!gl) {
+        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+        return;
+    }
+
+
+    // Vertex shader program
+
+    const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+    // Fragment shader program
+
+    const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+    // Initialize a shader program; this is where all the lighting
+    // for the vertices and so forth is established.
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    // Collect all the info needed to use the shader program.
+    // Look up which attribute our shader program is using
+    // for aVertexPosition and look up uniform locations.
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+        },
+    };
+
+
+    // Here's where we call the routine that builds all the
+    // objects we'll be drawing.
+    const buffers = initBuffers(gl);
+
+
+    let animationId = null;
+
+    const renderLoop = () => {
+        fps.render();
+
+        // for (let i = 0; i < 9; i++) {
+        universe.tick();
+        // }
+        drawGrid();
+        drawCells();
+
+        animationId = requestAnimationFrame(renderLoop);
+    };
+
+    const isPaused = () => {
+        return animationId === null;
+    };
+
+    const playPauseButton = document.getElementById("play-pause");
+    const destroyAllLife = document.getElementById("destroy-all-life");
+    const randomPopulation = document.getElementById("random-population");
+    playPauseButton.textContent = "❚❚";
+    destroyAllLife.textContent = "Destroy All Life";
+    randomPopulation.textContent = "Random Population";
+
+    const play = () => {
+        playPauseButton.textContent = "❚❚";
+        renderLoop();
+    };
+
+    const pause = () => {
+        playPauseButton.textContent = "▶";
+        cancelAnimationFrame(animationId);
+        animationId = null;
+    };
+
+    playPauseButton.addEventListener("click", event => {
+        if (isPaused()) {
+            play();
+        } else {
+            pause();
+        }
+    });
+
+    destroyAllLife.addEventListener("click", event => {
+        universe.destroy_all_life();
+        drawGrid();
+        drawCells();
+    });
+
+    randomPopulation.addEventListener("click", event => {
+        universe.random_population();
+        drawGrid();
+        drawCells();
+    });
+
+    const drawGrid = () => {
+        drawScene(gl, programInfo, buffers)
+
+        // ctx.beginPath();
+        // ctx.strokeStyle = GRID_COLOR;
+
+        // Vertical lines
+        for (let i = 0; i <= width; i++) {
+            // ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+            // ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+        }
+
+        // Horizontal lines
+        for (let j = 0; j <= width; j++) {
+            // ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+            // ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+        }
+
+        // ctx.stroke();
+    }
+
+    const getIndex = (row, column) => {
+        return row * width + column
+    };
+
+    const drawCells = () => {
+        const cellsPtr = universe.cells();
+        const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+        // ctx.beginPath();
+        //
+        // ctx.fillStyle = ALIVE_COLOR;
+        for (let row = 0; row < height; row++) {
+            for (let col = 0; col < width; col++) {
+                const idx = getIndex(row, col);
+                if (cells[idx] !== Cell.Alive) {
+                    continue;
+                }
+
+                // ctx.fillStyle = cells[idx] === Cell.Dead
+                //     ? DEAD_COLOR
+                //     : ALIVE_COLOR;
+                //
+                // ctx.fillRect(
+                //     col * (CELL_SIZE + 1) + 1,
+                //     row * (CELL_SIZE + 1) + 1,
+                //     CELL_SIZE,
+                //     CELL_SIZE
+                // );
+            }
+        }
+
+        // ctx.fillStyle = DEAD_COLOR;
+        for (let row = 0; row < height; row++) {
+            for (let col = 0; col < width; col++) {
+                const idx = getIndex(row, col);
+                if (cells[idx] !== Cell.Dead) {
+                    continue;
+                }
+
+                // ctx.fillStyle = cells[idx] === Cell.Dead
+                //     ? DEAD_COLOR
+                //     : ALIVE_COLOR;
+                //
+                // ctx.fillRect(
+                //     col * (CELL_SIZE + 1) + 1,
+                //     row * (CELL_SIZE + 1) + 1,
+                //     CELL_SIZE,
+                //     CELL_SIZE
+                // );
+            }
+        }
+
+        // ctx.stroke();
+    }
+
+    drawScene(gl, programInfo, buffers)
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(renderLoop);
+
+}
 
 const fps = new class {
     constructor() {
@@ -47,170 +274,178 @@ max of last 100 = ${Math.round(max)}
     }
 }
 
-const canvas = document.getElementById("game-of-life-canvas");
-canvas.height = (CELL_SIZE + 1) * height + 1;
-canvas.width = (CELL_SIZE + 1) * width + 1;
+//
+// initBuffers
+//
+// Initialize the buffers we'll need. For this demo, we just
+// have one object -- a simple two-dimensional square.
+//
+function initBuffers(gl) {
 
-canvas.addEventListener("click", event => {
-    const insertPopulation = document.getElementById("insert").value;
-    const hFlip = document.getElementById("h-flip").checked;
-    const vFlip = document.getElementById("v-flip").checked;
-    const invert = document.getElementById("invert").checked;
+    // Create a buffer for the square's positions.
 
-    const boundingRect = canvas.getBoundingClientRect();
+    const positionBuffer = gl.createBuffer();
 
-    const scaleX = canvas.width / boundingRect.width;
-    const scaleY = canvas.height / boundingRect.height;
+    // Select the positionBuffer as the one to apply buffer
+    // operations to from here out.
 
-    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    const row = (Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1) + height) % height;
-    const col = (Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1) + width) % width;
+    // Now create an array of positions for the square.
 
-    if (insertPopulation === "toggle") {
-        universe.toggle_cell(row, col);
-    } else {
-        universe.seed_population(row, col, insertPopulation, hFlip, vFlip, invert);
-    }
+    const positions = [
+        1.0,  .5,
+        -1.0,  .5,
+        1.0, -.5,
+        -1.0, -.5,
+    ];
 
-    drawGrid();
-    drawCells();
-});
+    // Now pass the list of positions into WebGL to build the
+    // shape. We do this by creating a Float32Array from the
+    // JavaScript array, then use it to fill the current buffer.
 
-const ctx = canvas.getContext('2d');
+    gl.bufferData(gl.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.STATIC_DRAW);
 
-let animationId = null;
-
-const renderLoop = () => {
-    fps.render();
-
-    // for (let i = 0; i < 9; i++) {
-        universe.tick();
-    // }
-    drawGrid();
-    drawCells();
-
-    animationId = requestAnimationFrame(renderLoop);
-};
-
-const isPaused = () => {
-    return animationId === null;
-};
-
-const playPauseButton = document.getElementById("play-pause");
-const destroyAllLife = document.getElementById("destroy-all-life");
-const randomPopulation = document.getElementById("random-population");
-playPauseButton.textContent = "❚❚";
-destroyAllLife.textContent = "Destroy All Life";
-randomPopulation.textContent = "Random Population";
-
-const play = () => {
-    playPauseButton.textContent = "❚❚";
-    renderLoop();
-};
-
-const pause = () => {
-    playPauseButton.textContent = "▶";
-    cancelAnimationFrame(animationId);
-    animationId = null;
-};
-
-playPauseButton.addEventListener("click", event => {
-    if (isPaused()) {
-        play();
-    } else {
-        pause();
-    }
-});
-
-destroyAllLife.addEventListener("click", event => {
-    universe.destroy_all_life();
-    drawGrid();
-    drawCells();
-});
-
-randomPopulation.addEventListener("click", event => {
-    universe.random_population();
-    drawGrid();
-    drawCells();
-});
-
-const drawGrid = () => {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-
-    // Vertical lines
-    for (let i = 0; i <= width; i++) {
-        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-    }
-
-    // Horizontal lines
-    for (let j = 0; j <= width; j++) {
-        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-    }
-
-    ctx.stroke();
+    return {
+        position: positionBuffer,
+    };
 }
 
-const getIndex = (row, column) => {
-    return row * width + column
-};
+//
+// Draw the scene.
+//
+function drawScene(gl, programInfo, buffers) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
-const drawCells = () => {
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+    // Clear the canvas before we start drawing on it.
 
-    ctx.beginPath();
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    ctx.fillStyle = ALIVE_COLOR;
-    for (let row = 0; row < height; row++) {
-        for (let col = 0; col < width; col++) {
-            const idx = getIndex(row, col);
-            if (cells[idx] !== Cell.Alive) {
-                continue;
-            }
+    // Create a perspective matrix, a special matrix that is
+    // used to simulate the distortion of perspective in a camera.
+    // Our field of view is 45 degrees, with a width/height
+    // ratio that matches the display size of the canvas
+    // and we only want to see objects between 0.1 units
+    // and 100 units away from the camera.
 
-            ctx.fillStyle = cells[idx] === Cell.Dead
-                ? DEAD_COLOR
-                : ALIVE_COLOR;
+    const fieldOfView = 45* Math.PI / 180;   // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
 
-            ctx.fillRect(
-                col * (CELL_SIZE + 1) + 1,
-                row * (CELL_SIZE + 1) + 1,
-                CELL_SIZE,
-                CELL_SIZE
-            );
-        }
+    // note: glmatrix.js always has the first argument
+    // as the destination to receive the result.
+    mat4.perspective(projectionMatrix,
+        fieldOfView,
+        aspect,
+        zNear,
+        zFar);
+
+    // Set the drawing position to the "identity" point, which is
+    // the center of the scene.
+    const modelViewMatrix = mat4.create();
+
+    // Now move the drawing position a bit to where we want to
+    // start drawing the square.
+
+    mat4.translate(modelViewMatrix,     // destination matrix
+        modelViewMatrix,     // matrix to translate
+        [-0.0, 0.0, -6.0]);  // amount to translate
+
+    // Tell WebGL how to pull out the positions from the position
+    // buffer into the vertexPosition attribute.
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexPosition);
     }
 
-    ctx.fillStyle = DEAD_COLOR;
-    for (let row = 0; row < height; row++) {
-        for (let col = 0; col < width; col++) {
-            const idx = getIndex(row, col);
-            if (cells[idx] !== Cell.Dead) {
-                continue;
-            }
+    // Tell WebGL to use our program when drawing
 
-            ctx.fillStyle = cells[idx] === Cell.Dead
-                ? DEAD_COLOR
-                : ALIVE_COLOR;
+    gl.useProgram(programInfo.program);
 
-            ctx.fillRect(
-                col * (CELL_SIZE + 1) + 1,
-                row * (CELL_SIZE + 1) + 1,
-                CELL_SIZE,
-                CELL_SIZE
-            );
-        }
+    // Set the shader uniforms
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix);
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix);
+
+    {
+        const offset = 0;
+        const vertexCount = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
     }
-
-    ctx.stroke();
 }
 
-drawGrid();
-drawCells();
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-requestAnimationFrame(renderLoop);
+    // Create the shader program
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // If creating the shader program failed, alert
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    // Send the source to the shader object
+
+    gl.shaderSource(shader, source);
+
+    // Compile the shader program
+
+    gl.compileShader(shader);
+
+    // See if it compiled successfully
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
